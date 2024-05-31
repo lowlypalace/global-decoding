c49d5fb83c4796bf7821b883d171f9aef8e90b2d
lowlypalace <lwlypalace@gmail.com>
2024-05-31 17:34:50 +0200

    Determenistic

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -19,6 +19,8 @@ def set_seed(seed):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    torch.use_deterministic_algorithms(True)
 
 
 @contextlib.contextmanager
