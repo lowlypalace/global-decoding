68bf6a6ebc7b0626b1a33445b0eb7b0ab9f0d082
lowlypalace <lwlypalace@gmail.com>
2024-06-02 18:04:04 +0200

    Optimize

--- src/sequences/generate_sequences_and_probs_hf.py
+++ src/sequences/generate_sequences_and_probs_hf.py
@@ -19,8 +19,14 @@ def generate_sequences_and_probs_hf(
 
     # Container for all generated sequences
     sequences_ids = []
-    target_logprobs_sums = []
-    proposal_logprobs_sums = []
+
+    # Placeholder for the log probability sums
+    target_logprob_sums = torch.tensor([], device=input_ids.device)
+    proposal_logprob_sums = torch.tensor([], device=input_ids.device)
+
+    # # Save the log probabilities for each token in the sequences
+    # proposal_logprobs_tokens = torch.tensor([], device=input_ids.device)
+    # target_logprobs_tokens = torch.tensor([], device=input_ids.device)
 
     with torch.no_grad():
         while len(sequences_ids) < sequence_count:
@@ -37,15 +43,14 @@ def generate_sequences_and_probs_hf(
                 return_dict_in_generate=True,
                 output_scores=True,
                 output_logits=True,
+                renormalize_logits=True
             )
 
             # Only use the IDs that were generated, excluding the input IDs
             gen_sequences_ids = sequences_batch.sequences[:, input_ids.shape[-1] :]
 
             logprobs_target = torch.stack(sequences_batch.logits, dim=1).log_softmax(-1)
-            logprobs_proposal = torch.stack(sequences_batch.scores, dim=1).log_softmax(
-                -1
-            )
+            logprobs_proposal = torch.stack(sequences_batch.scores, dim=1)
 
             selected_logprobs_proposal = torch.gather(
                 logprobs_proposal, 2, gen_sequences_ids[:, :, None]
@@ -61,11 +66,21 @@ def generate_sequences_and_probs_hf(
             # Apply the same mask to selected_logprobs_target
             selected_logprobs_target[mask_inf] = 0
 
+            # proposal_logprobs_tokens = torch.cat(
+            #     (proposal_logprobs_tokens, logprobs_proposal)
+            # )
+            # target_logprobs_tokens = torch.cat(
+            #     (target_logprobs_tokens, logprobs_target)
+            # )
+
             proposal_logprob_sum = torch.sum(selected_logprobs_proposal, dim=-1)
             target_logprob_sum = torch.sum(selected_logprobs_target, dim=-1)
 
-            proposal_logprobs_sums.extend(proposal_logprob_sum)
-            target_logprobs_sums.extend(target_logprob_sum)
+            # Append the results to the placeholders
+            target_logprob_sums = torch.cat((target_logprob_sums, target_logprob_sum))
+            proposal_logprob_sums = torch.cat(
+                (proposal_logprob_sums, proposal_logprob_sum)
+            )
 
             # Collect the generated sequences
             sequences_ids.extend(gen_sequences_ids)
@@ -90,12 +105,18 @@ def generate_sequences_and_probs_hf(
     sequences_ids = sequences_ids[:sequence_count]
     logging.info(f"Generated {len(sequences_ids)} sequence in total.")
 
+    # If we have more probabilities than needed due to the last batch, truncate the list
+    target_logprob_sums = target_logprob_sums[:sequence_count]
+    proposal_logprob_sums = proposal_logprob_sums[:sequence_count]
+
     # Decode sequences to text
     sequences_decoded = tokenizer.batch_decode(sequences_ids, skip_special_tokens=True)
 
     return (
         torch.stack(sequences_ids),
         sequences_decoded,
-        target_logprobs_sums,
-        proposal_logprobs_sums,
+        target_logprob_sums,
+        proposal_logprob_sums,
+        # proposal_logprobs_tokens,
+        # target_logprobs_tokens,
     )
