1a4954464b4cae820596db345d9fa2dc54ba1138
lowlypalace <lwlypalace@gmail.com>
2024-06-05 18:20:43 +0200

    Edit

--- src/sequences/sequences_probs.py
+++ src/sequences/sequences_probs.py
@@ -18,15 +18,13 @@ def top_p_filtering(logits, top_p):
     Applies top-p filtering to the logits to enforce more focused and coherent text generation.
 
     Args:
-        scores: The original logits from the model output.
-        top_p: The cumulative probability cutoff for token selection. Should be between 0 and 1.
-        filter_value: The value to replace filtered logits with.
+        logits torch.FloatTensor: The original logits from the model output.
+        top_p (float): The cumulative probability cutoff for token selection. Should be between 0 and 1.
+        filter_value (float): The value to replace filtered logits with.
 
     Returns:
         torch.FloatTensor: The adjusted logits after applying top-p filtering.
     """
-    # if top_p < 0 or top_p > 1.0:
-    #     raise ValueError(f"`top_p` has to be a float > 0 and < 1, but is {top_p}")
     # Sort the logits to identify the cut-off threshold
     sorted_logits, sorted_indices = torch.sort(logits, descending=True)
     cumulative_probs = torch.softmax(sorted_logits, dim=-1).cumsum(dim=-1)
