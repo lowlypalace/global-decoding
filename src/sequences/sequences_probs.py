4ef8e0a60e6a6ef420d4a3fd8d6c17fb64f90721
lowlypalace <lwlypalace@gmail.com>
2024-06-06 13:37:58 +0200

    Comment

--- src/sequences/sequences_probs.py
+++ src/sequences/sequences_probs.py
@@ -46,7 +46,6 @@ def mask_out_pad_token(log_probs, index, pad_token_id):
     return log_probs
 
 
-# def get_logprobs(logits, index, pad_token_id, top_k=None, top_p=None):
 def get_logprobs(logits, index, pad_token_id, top_k=None, top_p=None):
     # If top_k is specified, apply top-k filtering
     if top_k is not None:
