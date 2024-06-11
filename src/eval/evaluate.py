0fda0a35c54efc7addd4679020534acd6f36965b
lowlypalace <lwlypalace@gmail.com>
2024-06-11 04:41:14 +0200

    Generic

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -88,6 +88,10 @@ def evaluate_mauve(args, output_subdir, local_decoding_texts, global_decoding_te
 
 
 def evaluate_bleu(args, output_subdir, local_decoding_texts, global_decoding_texts):
+    def contains_only_nonprintable(text):
+        # Check if all characters in the text are non-printable
+        return all(not char.isprintable() for char in text.strip())
+
     def compute_self_bleu(texts):
         experiment_id = secrets.token_hex(3)
         # Load the BLEU metric
@@ -124,12 +128,12 @@ def evaluate_bleu(args, output_subdir, local_decoding_texts, global_decoding_tex
     local_decoding_texts = local_decoding_texts[:eval_num_sequences]
     global_decoding_texts = global_decoding_texts[:eval_num_sequences]
 
-    # Remove texts with form feed character (chr(12)) from global decoding texts
+    # Remove texts that contain only non-printable characters
     # As they raise divide by zero error in BLEU computation
     # https://github.com/huggingface/evaluate/issues/601
-    global_decoding_texts = [text for text in global_decoding_texts if text != chr(12)]
+    global_decoding_texts = [text for text in global_decoding_texts if not contains_only_nonprintable(text)]
     logging.warning(
-        f"Removed {len(local_decoding_texts) - len(global_decoding_texts)} texts with form feed character from global decoding texts."
+        f"Removed {len(local_decoding_texts) - len(global_decoding_texts)} texts with non-printable characters"
     )
 
     # Compute Self-BLEU for local decoding texts
