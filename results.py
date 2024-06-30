b04fe8c70a4987e70464e3f533a66d661e7173ff
lowlypalace <lwlypalace@gmail.com>
2024-06-30 09:33:42 +0200

    Parse args

--- results.py
+++ results.py
@@ -342,30 +342,82 @@ def save_results(top_k_df, top_p_df, model_name, results_dir):
     top_p_df.to_csv(os.path.join(results_dir, f"top_p_{model_name}.csv"), sep="\t")
 
 
+
+# Define the function to parse command-line arguments
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generate results for the experiments."
+    )
+
+    parser.add_argument(
+        "--model_names",
+        nargs="+",
+        default=[
+            "pythia-70m",
+            "pythia-410m",
+            "pythia-1.4b",
+            # "pythia-2.8b",
+        ],
+        choices=[
+            "gpt2",
+            "gpt2-medium",
+            "gpt2-large",
+            "gpt2-xl",
+            "pythia-70m",
+            "pythia-160m",
+            "pythia-410m",
+            "pythia-1b",
+            "pythia-1.4b",
+            "pythia-2.8b",
+            "pythia-6.9b",
+            "pythia-12b",
+        ],
+        help="Models to use for generating results.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="Random seed for reproducibility.",
+    )
+    parser.add_argument(
+        "--results_dir",
+        type=str,
+        default="results",
+        help="Directory to save the results files.",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
 def main():
 
-    results_dir = "results"  # TODO: Accept as arg
+    args = parse_args()
+
+    # Set seed for random number generators
+    random.seed(args.seed)
 
     # Create a directory to save the output files
-    os.makedirs(results_dir, exist_ok=True)
+    os.makedirs(args.results_dir, exist_ok=True)
 
     # model_names = ['pythia-70m', 'pythia-410m', 'pythia-1.4b' , 'pythia-2.8b']
-    model_names = ['pythia-70m', 'pythia-410m', 'pythia-1.4b'] # TODO: Accept as arg
+    model_names = ['pythia-70m', 'pythia-410m', 'pythia-1.4b']
 
     results = {}
 
-    for model_name in model_names:
-        top_k_df, top_p_df = get_results(model_name)
+    for model_name in args.model_names:
+        top_k_df, top_p_df = get_results(model_name, args)
 
-        save_results(top_k_df, top_p_df, model_name, results_dir)
+        save_results(top_k_df, top_p_df, model_name, args.results_dir)
 
         results[model_name] = {
             'top_k': top_k_df,
             'top_p': top_p_df
         }
 
-    plot_sequences_lengths(results, results_dir)
-    plot_average_log_likelihood(results, results_dir)
+    plot_sequences_lengths(results, args.results_dir)
+    plot_average_log_likelihood(results, args.results_dir)
 
     # TODO: plot MAUVE / BLEU for each model
 
